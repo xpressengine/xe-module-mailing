879fa1000018ee9d42b92890e1437063eaa6c783
royallin <royallin@fd9b9610-727b-78e2-62dc-b0717bfe16b8>
2010-04-25 04:01:06 +0000

    Add TW language pack (progressing)
    git-svn-id: http://xe-mailing.googlecode.com/svn/trunk/modules/mailing@7 fd9b9610-727b-78e2-62dc-b0717bfe16b8

---
+++ lang/zh-TW.lang.php
@@ -0,0 +1,37 @@
+<?php
+    $lang->mailing_list = '郵件列表';
+    $lang->about_mailing_list = '메일과 문서 연동을 통해 웹사이트에서만 글/ 댓글을 쓰고 조회하는 것이 아니라 쓰여지는 글/ 댓글이 메일로 발송되고 해당 메일의 답메일을 통해 웹사이트에 등록될 수 있는 모듈입니다.';
+
+    $lang->mailing_notice = '회원 로그인을 하시면 원하시는 게시판의 메일링 가입을 하실 수 있습니다.<br/>메일링 리스트 가입을 하게 되면 해당 게시판에 신규 글/댓글이 작성될 때 메일로 받을 수 있고 메일로 게시판에 글/ 댓글을 등록하실 수 있습니다.';
+    $lang->mailing_create_info = '원하시는 주제의 게시판이 없으시다면 새로운 주제의 게시판을 만드세요.<br/>만들기 버튼을 선택하시면 원하는 주제의 게시판을 만드실 수 있습니다.';
+
+    $lang->mailing_theme = '주제별 게시판';
+
+    $lang->mailing_domain = '메일 도메인 주소';
+    $lang->about_mailing_domain = '메일링 모듈은 mid@도메인 또는 vid.mid@도메인 과 같이 메일 도메인이 부여되어 있어야 합니다<br/>설정된 메일 도메인을 등록해주세요.';
+    $lang->mailing_display_board_header = '게시판 상단 메일링 정보 출력';
+    $lang->about_mailing_display_board_header = '게시판의 상단에 메일링과 관련된 정보(게시판의 메일 주소, 가입여부)등을 출력할 수 있습니다';
+    $lang->mailing_target_module = '선택 모듈 최신글 출력';
+    $lang->mailing_target_module_count = '게시글 출력 수';
+    $lang->about_mailing_target_module = '메일링 모듈 서비스 페이지에 선택된 모듈들은 개별 최신글이 출력됩니다';
+
+    $lang->mailing_display_count = '통합 최신글 출력 수';
+    $lang->about_mailing_display_count = '게시판, 위키모듈등 문서 모듈을 사용하는 모든 모듈의 최신글을 출력 수를 설정할 수 있습니다. (預設 20)';
+
+    $lang->mailing_joined_newest_documents = '가입한 게시판 최신 글';
+    $lang->mailing_newest_documents = '最新主題';
+
+    $lang->mailing_info_not_logged = '로그인 하시면 <strong>%s</strong> 메일링 가입을 하실 수 있습니다.';
+    $lang->mailing_info = '<strong>%s</strong> 的郵件列表';
+    $lang->about_mailing_info= '이 게시판의 메일링 기능을 사용하지 않고 있습니다.<br/>우측 아이콘을 선택하셔서 활성화 시키시면 메일링 기능을 사용하실 수 있습니다.<br/>메일링 기능 사용시 이 게시판에 새 글/ 댓글이 등록이 되면 메일로 받을 수 있습니다.<br/>또는 <strong>%s</strong>로 메일을 보내면 게시판에 접속하지 않고 새 글을 등록할 수 있고 답변 메일을 보내면 원본 글에 댓글로 등록이 될 수 있습니다.';
+    $lang->about_mailing_info_joined = '이 게시판의 메일링 기능을 사용하고 계십니다.<br/>사용하지 않으시려면 우측 아이콘을 선택하셔서 비활성화 시키시면 됩니다.<br/>이 게시판으로 글을 쓰기 위해서는 <strong>[module_address]</strong> 로 메일을 작성/ 발송하시면 됩니다<br/>단, 발송 메일 주소가 <strong>[mail_address]</strong> 이어야 합니다.';
+    $lang->about_mailing_count = '이 게시판의 메일링 참가자 수는 %d 명입니다.';
+
+    $lang->cmd_create = '建立';
+    $lang->board_id = '郵件位址';
+
+    $lang->mailing_create_mid = '주소';
+    $lang->about_mailing_create_mid = '접속 URL을 정하실 수 있습니다. 3~10자 사이로 영문으로 시작하고 영문,숫자,_(언더바) 사용이 가능합니다';
+    $lang->mailing_create_title = '討論板名稱';
+    $lang->about_mailing_create_title = '표시될 게시판의 이름을 정할 수 있습니다. 가능하면 짧고 이해가 쉬운 이름을 정해주세요. (1~10個字之間)';
+?>
