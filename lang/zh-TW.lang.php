bd296e3e17fa7fa403e58dabb8791d7b209735c6
royallin <royallin@fd9b9610-727b-78e2-62dc-b0717bfe16b8>
2010-05-30 09:24:26 +0000

    Update Taiwanese (progressing)
    git-svn-id: http://xe-mailing.googlecode.com/svn/trunk/modules/mailing@16 fd9b9610-727b-78e2-62dc-b0717bfe16b8

--- lang/zh-TW.lang.php
+++ lang/zh-TW.lang.php
@@ -3,16 +3,16 @@
     $lang->about_mailing_list = '메일과 문서 연동을 통해 웹사이트에서만 글/ 댓글을 쓰고 조회하는 것이 아니라 쓰여지는 글/ 댓글이 메일로 발송되고 해당 메일의 답메일을 통해 웹사이트에 등록될 수 있는 모듈입니다.';
 
     $lang->mailing_notice = '회원 로그인을 하시면 원하시는 게시판의 메일링 가입을 하실 수 있습니다.<br/>메일링 리스트 가입을 하게 되면 해당 게시판에 신규 글/댓글이 작성될 때 메일로 받을 수 있고 메일로 게시판에 글/ 댓글을 등록하실 수 있습니다.';
-    $lang->mailing_create_info = '원하시는 주제의 게시판이 없으시다면 새로운 주제의 게시판을 만드세요.<br/>만들기 버튼을 선택하시면 원하는 주제의 게시판을 만드실 수 있습니다.';
+    $lang->mailing_create_info = '원하시는 주제의 게시판이 없으시다면 새로운 주제의 게시판을 만드세요.<br/>按『建立』按鈕，可建立自己的討論板。';
 
     $lang->mailing_theme = '주제별 게시판';
 
-    $lang->mailing_domain = '메일 도메인 주소';
+    $lang->mailing_domain = '郵件網域位址';
     $lang->about_mailing_domain = '메일링 모듈은 mid@도메인 또는 vid.mid@도메인 과 같이 메일 도메인이 부여되어 있어야 합니다<br/>설정된 메일 도메인을 등록해주세요.';
-    $lang->mailing_display_board_header = '게시판 상단 메일링 정보 출력';
+    $lang->mailing_display_board_header = '討論板上方顯示郵件資料';
     $lang->about_mailing_display_board_header = '게시판의 상단에 메일링과 관련된 정보(게시판의 메일 주소, 가입여부)등을 출력할 수 있습니다';
-    $lang->mailing_target_module = '선택 모듈 최신글 출력';
-    $lang->mailing_target_module_count = '게시글 출력 수';
+    $lang->mailing_target_module = '選擇目標模組';
+    $lang->mailing_target_module_count = '目標模組數量';
     $lang->about_mailing_target_module = '메일링 모듈 서비스 페이지에 선택된 모듈들은 개별 최신글이 출력됩니다';
 
     $lang->mailing_display_count = '통합 최신글 출력 수';
@@ -21,21 +21,21 @@
     $lang->mailing_joined_newest_documents = '가입한 게시판 최신 글';
     $lang->mailing_newest_documents = '最新主題';
 
-    $lang->mailing_info_not_logged = '로그인 하시면 <strong>%s</strong> 메일링 가입을 하실 수 있습니다.';
+    $lang->mailing_info_not_logged = '請先登入 <strong>%s</strong> 才可訂閱郵件。';
     $lang->mailing_info = '<strong>%s</strong>';
-    $lang->about_mailing_info= '<strong class="hx">귀하는 이 게시판의 메일링 기능을 사용하지 않고 있습니다.</strong><br/><strong>메일링 신청</strong> 버튼을 누르면 메일링 기능을 사용할 수 있습니다.<br/>메일링 기능을 사용하면 이 게시판에 새 글 및 댓글이 등록될 때 메일로 받을 수 있습니다.<br/><strong>%s</strong>으로 메일을 보내면 게시판에 접속하지 않고 새 글을 등록할 수 있으며 답변 메일을 보내면 원본 글에 댓글로 등록이 됩니다.';
-    $lang->about_mailing_info_joined = '<strong class="hx">귀하는 이 게시판의 메일링 기능을 사용하고 있습니다.</strong><br/><strong>메일링 해지</strong> 버튼을 누르면 메일링 기능을 중단할 수 있습니다.<br/><strong>[module_address]</strong>으로 메일을 보내면 게시판에 접속하지 않고 새 글을 등록할 수 있으며 답변 메일을 보내면 원본 글에 댓글로 등록이 됩니다.<br/>메일링에 사용하는 발송 메일의 주소는 <strong>[mail_address]</strong> 이어야 합니다.';
-    $lang->about_mailing_count = '이 게시판의 메일링 신청자 수는 현재 <strong>%d</strong> 명입니다.';
+    $lang->about_mailing_info= '<strong class="hx">此討論板的郵件功能使用中。</strong><br/>按 <strong>訂閱郵件</strong> 按鈕之後就能使用郵件功能。<br/>使用此功能後，當有新主題或評論時將會發送郵件到所註冊的信箱中。<br/><strong>%s</strong>으로 메일을 보내면 게시판에 접속하지 않고 새 글을 등록할 수 있으며 답변 메일을 보내면 원본 글에 댓글로 등록이 됩니다.';
+    $lang->about_mailing_info_joined = '<strong class="hx">此討論板的郵件功能使用中。</strong><br/>按 <strong>取消訂閱</strong> 按鈕之後就能停止使用郵件功能。<br/><strong>[module_address]</strong>으로 메일을 보내면 게시판에 접속하지 않고 새 글을 등록할 수 있으며 답변 메일을 보내면 원본 글에 댓글로 등록이 됩니다.<br/>所發送的郵件位址 <strong>[mail_address]</strong>';
+    $lang->about_mailing_count = '目前此討論板訂閱人數為 <strong>%d</strong> 人。';
 
     $lang->cmd_create = '建立';
     $lang->board_id = '郵件位址';
 
-    $lang->mailing_create_mid = '주소';
-    $lang->about_mailing_create_mid = '접속 URL을 정하실 수 있습니다. 3~10자 사이로 영문으로 시작하고 영문,숫자,_(언더바) 사용이 가능합니다';
+    $lang->mailing_create_mid = '位址';
+    $lang->about_mailing_create_mid = '請指定要連結的網址名稱。 以英文開頭 3~10 個字，只能使用英文、數字與底線。';
     $lang->mailing_create_title = '討論板名稱';
-    $lang->about_mailing_create_title = '표시될 게시판의 이름을 정할 수 있습니다. 가능하면 짧고 이해가 쉬운 이름을 정해주세요. (1~10個字之間)';
+    $lang->about_mailing_create_title = '請指定要顯示的討論板名稱，名稱盡量簡短。 (1~10個字之間)';
 	
-	$lang->mailing_optin = '<strong>%s</strong> 메일링 신청';
-	$lang->mailing_optout = '<strong>%s</strong> 메일링 해지';
-	$lang->mailing_close = '메일링 도움말 닫기';
+	$lang->mailing_optin = '<strong>%s</strong> 訂閱郵件';
+	$lang->mailing_optout = '<strong>%s</strong> 取消訂閱';
+	$lang->mailing_close = '關閉';
 ?>
